fe9135a86d89a143f272d7a96e9f0ebacd4be87c
Simiao Carvalho <simiaosimis@gmail.com>
2015-09-16 00:05:20 -0300

    Fix user order

--- app/controllers/uri_query_controller.rb
+++ app/controllers/uri_query_controller.rb
@@ -2,15 +2,14 @@ require 'open-uri'
 
 class UriQueryController < ApplicationController
 	def index
-		page_url = "https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=20&page=show_authorstats&userid=785687"
+		page_url = "https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=20&page=show_authorstats&userid=718101"
 		page = Nokogiri::HTML(open(page_url))
 		@questions_d1 = page.xpath("//table")
 		page_url = "https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=20&page=show_authorstats&userid=785641"
 		page = Nokogiri::HTML(open(page_url))
 		@questions_d2 = page.xpath("//table")
-		page_url = "https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=20&page=show_authorstats&userid=718101"
+		page_url = "https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=20&page=show_authorstats&userid=785687"
 		page = Nokogiri::HTML(open(page_url))
 		@questions_d3 = page.xpath("//table")
-		#@questions = page.css('table:nth-child(3)')
 	end
 end
