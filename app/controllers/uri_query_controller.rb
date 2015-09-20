df98a211c6e3a983334bb34e84905fb7de6b8aa2
Simiao Carvalho <simiaosimis@gmail.com>
2015-09-20 15:55:10 -0300

    Fix name mistake

--- app/controllers/uri_query_controller.rb
+++ app/controllers/uri_query_controller.rb
@@ -5,9 +5,9 @@ class UriQueryController < ApplicationController
 
 		@players = []
 
-		update_player "718101", @players
-		update_player "785641", @players
-		update_player "785687", @players
+		update_player "718101", @players, "Duente_1"
+		update_player "785641", @players, "Duente_2"
+		update_player "785687", @players, "Duente_3"
 
 		@players.sort!
 	end
@@ -17,16 +17,16 @@ class UriQueryController < ApplicationController
 		return page.xpath("//div[contains(@align, 'center')]/table/tr/td/h1/text()").collect {|node| node.text.strip}
 	end
 
-	def update_player (id, array)
+	def update_player (id, array, name)
 		if(player = Player.find_by_id(id))
 			data = get_data "https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=20&page=show_authorstats&userid=#{id}"
-			player.update(:name => "Duente_2", :submissions => data[0], :tried => data[1], :solved => data[2],
-			:first_sub => data[3], :last_sub => data[4])
+			player.update(:name => name, :submissions => data[0], :tried => data[1], :solved => data[2],
+			:last_sub => data[4])
 			player.save!
 			array << player
 		else
 			data = get_data "https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=20&page=show_authorstats&userid=#{id}"
-			player = Player.new(:id => id, :name => "Duente_2", :submissions => data[0], :tried => data[1], :solved => data[2],
+			player = Player.new(:id => id, :name => name, :submissions => data[0], :tried => data[1], :solved => data[2],
 			:first_sub => data[3], :last_sub => data[4])
 			player.save!
 			array << player
